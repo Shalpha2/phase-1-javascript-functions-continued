f5470c8b3085f6bddd7a3d7c8514d4a188a5dda2
Shadrack Kiprono <shadrackkiprono00@gmail.com>
2025-03-05 08:08:16 -0800

    function continued

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+// Function declaration for saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function expression for mondayWork
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
